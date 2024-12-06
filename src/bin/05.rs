51810dde2501bb1b5320710b9438a214f7f39ce3
bricked <git@bricked.dev>
2024-12-06 21:48:39 +0100

    style: format

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -9,11 +9,11 @@ pub fn part_one(input: &str) -> Option<u32> {
         while update.len() > 0 {
             for rule in rules.iter().filter(|r| r.1 == update[0]) {
                 if update.contains(&rule.0) {
-                    return false
+                    return false;
                 }
             }
             update = &update[1..];
-        };
+        }
         true
     });
 
@@ -26,13 +26,17 @@ pub fn parse_input(input: &str) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
 
     for line in input.lines() {
         if line.contains("|") {
-            let rule = line.split("|").map(|n| n.parse().unwrap()).collect_tuple().unwrap();
+            let rule = line
+                .split("|")
+                .map(|n| n.parse().unwrap())
+                .collect_tuple()
+                .unwrap();
             rules.push(rule);
         } else if line.contains(",") {
             let update = line.split(",").map(|n| n.parse().unwrap()).collect();
             updates.push(update);
         }
-    };
+    }
     (rules, updates)
 }
 
@@ -54,8 +58,12 @@ pub fn part_two(input: &str) -> Option<u32> {
                 }
             }
             update_slice = &mut update_slice[1..];
-        };
-        if is_valid { None } else { Some(update) }
+        }
+        if is_valid {
+            None
+        } else {
+            Some(update)
+        }
     });
 
     Some(corrected_updates.map(|u| u[u.len() / 2]).sum())
--- src/bin/06.rs
+++ src/bin/06.rs
@@ -5,39 +5,53 @@ advent_of_code::solution!(6);
 pub fn part_one(input: &str) -> Option<u32> {
     let mut lab: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     walk(&mut lab);
-    Some(lab.iter().flatten().filter(|&&c| "^>v<".contains(c)).count() as u32)
+    Some(
+        lab.iter()
+            .flatten()
+            .filter(|&&c| "^>v<".contains(c))
+            .count() as u32,
+    )
 }
 
 pub fn walk(lab: &mut Vec<Vec<char>>) -> bool {
-    let mut position = lab.iter().enumerate().find_map(|(y, row)| {
-        let x = row.iter().position(|&char| char == '^');
-        match x {
-            Some(x) => Some(Position{x, y}),
-            None => None
-        }
-    }).unwrap();
+    let mut position = lab
+        .iter()
+        .enumerate()
+        .find_map(|(y, row)| {
+            let x = row.iter().position(|&char| char == '^');
+            match x {
+                Some(x) => Some(Position { x, y }),
+                None => None,
+            }
+        })
+        .unwrap();
 
-
-    let mut direction = Direction{x: 0, y: -1};
+    let mut direction = Direction { x: 0, y: -1 };
     'walk: loop {
         lab[position.y][position.x] = direction_indicator(&direction);
         'turn: loop {
-            let next_position = Position{x: position.x.wrapping_add_signed(direction.x), y: position.y.wrapping_add_signed(direction.y)};
+            let next_position = Position {
+                x: position.x.wrapping_add_signed(direction.x),
+                y: position.y.wrapping_add_signed(direction.y),
+            };
             if next_position.x >= lab.len() || next_position.y >= lab[0].len() {
                 break 'walk;
             }
             if lab[next_position.y][next_position.x] == '#' {
-                direction = Direction{x: -direction.y, y: direction.x};
+                direction = Direction {
+                    x: -direction.y,
+                    y: direction.x,
+                };
             } else {
                 position = next_position;
-                break 'turn
+                break 'turn;
             }
         }
         let indicator = direction_indicator(&direction);
         if indicator == lab[position.y][position.x] {
             return true;
         }
-    };
+    }
     false
 }
 
@@ -53,21 +67,25 @@ pub struct Direction {
 pub fn part_two(input: &str) -> Option<u32> {
     let lab: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     let mut loops = 0u32;
-    for obstacle in iproduct!(0..lab.len(), 0..lab[0].len()).map(|(y, x)| Position{x, y}) {
-        if lab[obstacle.y][obstacle.x] != '.' {continue;};
+    for obstacle in iproduct!(0..lab.len(), 0..lab[0].len()).map(|(y, x)| Position { x, y }) {
+        if lab[obstacle.y][obstacle.x] != '.' {
+            continue;
+        };
         let mut obstructed_lab = lab.clone();
         obstructed_lab[obstacle.y][obstacle.x] = '#';
-        if walk(&mut obstructed_lab) {loops += 1};
+        if walk(&mut obstructed_lab) {
+            loops += 1
+        };
     }
     Some(loops)
 }
 
 pub fn direction_indicator(direction: &Direction) -> char {
     match direction {
-        Direction{x: 0, y: -1} => '^',
-        Direction{x: 0, y: 1} => 'v',
-        Direction{x: -1, y: 0} => '<',
-        Direction{x: 1, y: 0} => '>',
+        Direction { x: 0, y: -1 } => '^',
+        Direction { x: 0, y: 1 } => 'v',
+        Direction { x: -1, y: 0 } => '<',
+        Direction { x: 1, y: 0 } => '>',
         _ => 'X',
     }
 }
